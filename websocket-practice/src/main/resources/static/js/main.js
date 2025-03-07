d5c97076a180f71e286a91b9dd464b76782a50bb
Zhomart <zturtan@gmail.com>
2025-03-07 23:46:00 +0500

    adding some methods to main.js like connecting and finding online users

--- websocket-practice/src/main/resources/static/js/main.js
+++ websocket-practice/src/main/resources/static/js/main.js
@@ -1,45 +1,86 @@
-    'use strict';
-    const usernamePage = document.querySelector('#username-page');
-    const chatPage = document.querySelector('#chat-page');
-    const usernameForm = document.querySelector('#usernameForm');
-    const messageForm = document.querySelector('#messageForm');
-    const messageInput = document.querySelector('#message');
-    const connectingElement = document.querySelector('.connecting');
-    const chatArea = document.querySelector('#chat-messages');
-    const logout = document.querySelector('#logout');
+'use strict';
+const usernamePage = document.querySelector('#username-page');
+const chatPage = document.querySelector('#chat-page');
+const usernameForm = document.querySelector('#usernameForm');
+const messageForm = document.querySelector('#messageForm');
+const messageInput = document.querySelector('#message');
+const connectingElement = document.querySelector('.connecting');
+const chatArea = document.querySelector('#chat-messages');
+const logout = document.querySelector('#logout');
 
-    let stompClient = null;
-    let nickname = null;
-    let fullname = null;
-    let selectedUserId = null;
+let stompClient = null;
+let nickname = null;
+let fullname = null;
+let selectedUserId = null;
 
-    function connect(event) {
-        nickname  = document.querySelector('#nickname').value.trim()
-        fullname  = document.querySelector('#fullname').value.trim()
+function connect(event) {
+    nickname = document.querySelector('#nickname').value.trim()
+    fullname = document.querySelector('#fullname').value.trim()
 
-        if(nickname&&fullname){
-            usernamePage.classList.add('hidden');
-            chatPage.classList.add('hidden');
-
-
-            const socket = new SockJS('/ws');
-            stompClient = Stomp.over(socket);
-
-            stompClient.connect({}, onConnected, onError);
+    if (nickname && fullname) {
+        usernamePage.classList.add('hidden');
+        chatPage.classList.add('hidden');
 
 
+        const socket = new SockJS('/ws');
+        stompClient = Stomp.over(socket);
 
-        }
+        stompClient.connect({}, onConnected, onError);
 
-        event.preventDefault();
+
+    }
+
+    event.preventDefault();
+
+
+}
+
+function onConnected() {
+    stompClient.subscribe(`/user/${nickname}/queue/message`, onMessageReceived);
+    stompClient.subscribe(`/user/topic`, onMessageReceived);
+
+    stompClient.send('app/user.addUser'
+        , {},
+        JSON.stringify({nickname: nickname, fullname: fullname, status: 'ONLINE'})
+
+    );
+    document.querySelector('#connected-user-fullname').textContent = fullname;
+    findAndDisplayConnectedUsers().then();
+
+
+
+
+
+}
+    async function findAndDisplayConnectedUsers(){
+        const connectedUsersResponse = await fetch('/users');
+        let connectedUsers = await connectedUsersResponse.json();
+        connectedUsers  = connectedUsers.filter(user  => user.nickname !== nickname);
+        const  connectedUsersList = document.getElementById('connectedUsers');
+        connectedUsersList.innerHTML =  '';
+        connectedUsers.forEach(user => {
+            appendUserElement(user, connectedUsersList);
+            if(connectedUsers.indexOf(user)< connectedUsers.length -1){
+                const separator = document.createElement('li');
+                separator.classList.add('separator');
+                connectedUsers.appendChild(separator);
+            }
+
+        })
+
+
 
 
     }
 
 
+function onError() {
 
 
-    usernameForm.addEventListener('submit', connect, true); // step 1
-    messageForm.addEventListener('submit', sendMessage, true);
-    logout.addEventListener('click', onLogout, true);
-    window.onbeforeunload = () => onLogout();
+}
+
+
+usernameForm.addEventListener('submit', connect, true); // step 1
+messageForm.addEventListener('submit', sendMessage, true);
+logout.addEventListener('click', onLogout, true);
+window.onbeforeunload = () => onLogout();
